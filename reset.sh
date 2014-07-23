f863d579545b09f458a750964dc98623be8ae3db
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-07-23 14:31:40 +0200

    Touch .ocdata file because ownCloud demands it

--- reset.sh
+++ reset.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd "$DIR"
 
 mkdir -p owncloud/data/
+touch owncloud/data/.ocdata
 mkdir -p owncloud/config/
 cp config/owncloud.config.php owncloud/config/config.php
 cp config/owncloud.db owncloud/data/owncloud.db
