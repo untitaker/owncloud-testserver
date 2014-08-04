8b8e843d5bb4ce23d01e5fecbd77d8bad93d2e68
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-08-04 19:12:53 +0200

    ownCloud 7.0.1

--- owncloud/version.php
+++ owncloud/version.php
@@ -1,6 +1,6 @@
 <?php 
-$OC_Version = array(7,0,0,8);
-$OC_VersionString = '7.0.0';
+$OC_Version = array(7,0,1,1);
+$OC_VersionString = '7.0.1';
 $OC_Edition = '';
 $OC_Channel = 'stable';
-$OC_Build = '2014-07-22T20:02:34+00:00';
+$OC_Build = '2014-08-04T13:49:17+00:00';
