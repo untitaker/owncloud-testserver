99155f1941c766ecf2dbe22f4f81bd0345730b64
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-08-18 23:01:38 +0200

    Improve logging

--- php.sh
+++ php.sh
@@ -3,4 +3,5 @@ set -e
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "$DIR is the own directory."
 cd $DIR/owncloud/
+tail -F -n0 data/owncloud.log &
 exec php -c $DIR/config/php.ini -S 127.0.0.1:8080
