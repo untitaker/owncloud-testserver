b852b966e8e7825e134aa9b3ffa11decda494e4f
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-08-18 22:54:44 +0200

    Fix instance directory

--- config/owncloud.config.php
+++ config/owncloud.config.php
@@ -7,7 +7,6 @@ $CONFIG = array (
   array (
     0 => '127.0.0.1:8080',
   ),
-  'datadirectory' => '/home/untitaker/projects/owncloud-testserver/owncloud/data',
   'overwrite.cli.url' => 'http://127.0.0.1:8080',
   'dbtype' => 'sqlite3',
   'version' => '8.1.1.3',
