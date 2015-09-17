954409202ba0f8f33b8defefb7f48046e3d3ed1a
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-09-17 13:00:30 +0200

    Fix termination of jobs in php.sh

--- php.sh
+++ php.sh
@@ -3,5 +3,8 @@ set -e
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "$DIR is the own directory."
 cd $DIR/owncloud/
+
+trap 'kill $(jobs -p) &> /dev/null' TERM EXIT
 tail -F -n0 data/owncloud.log &
-exec php -c $DIR/config/php.ini -S 127.0.0.1:8080
+php -c $DIR/config/php.ini -S 127.0.0.1:8080 &
+wait
