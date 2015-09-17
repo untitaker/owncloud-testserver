66542fcdf9e93f49a8063c1e7013b742e6059090
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-09-17 11:36:25 +0200

    ownCloud 8.1.3

--- config/owncloud.config.php
+++ config/owncloud.config.php
@@ -5,11 +5,13 @@ $CONFIG = array (
   'secret' => 'cGV0cMIPDHfCswmfR7ECtMN99tEg/7osnVeyGOvJHZb7hiAs',
   'trusted_domains' => 
   array (
-    0 => '127.0.0.1:8080',
+    0 => '127.0.0.1',
   ),
   'overwrite.cli.url' => 'http://127.0.0.1:8080',
   'dbtype' => 'sqlite3',
-  'version' => '8.1.1.3',
+  'version' => '8.1.3.0',
   'logtimezone' => 'UTC',
   'installed' => true,
+  'theme' => '',
+  'maintenance' => false,
 );
