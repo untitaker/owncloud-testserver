6a61007bbd2a4c56d360a4343684b8c1f5dd8428
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-09-17 16:12:54 +0200

    Fix Travis builds

--- php.sh
+++ php.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-set -e
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 echo "$DIR is the own directory."
 cd $DIR/owncloud/
 
-trap 'kill $(jobs -p) &> /dev/null' TERM EXIT
 tail -F -n0 data/owncloud.log &
-php -c $DIR/config/php.ini -S 127.0.0.1:8080 &
-wait
+php -c $DIR/config/php.ini -S 127.0.0.1:8080
+kill $(jobs -p) &> /dev/null
