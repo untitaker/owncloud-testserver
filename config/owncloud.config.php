797304c9d494c4d3811bcd387fa9551f872df2d1
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-10-20 17:12:57 +0200

    db upgrade

--- config/owncloud.config.php
+++ config/owncloud.config.php
@@ -5,13 +5,15 @@ $CONFIG = array (
   'secret' => 'IsWcIXIEKzxwoIn+WXKYSIInkOdLBLAdSuhKlePNkHHcgY+J',
   'trusted_domains' => 
   array (
-    0 => '127.0.0.1:8080',
+    0 => '127.0.0.1',
   ),
   'overwrite.cli.url' => 'http://127.0.0.1:8080',
   'dbtype' => 'sqlite3',
-  'version' => '8.1.3.0',
+  'version' => '8.2.0.12',
   'logtimezone' => 'UTC',
   'installed' => true,
   'theme' => '',
   'maintenance' => false,
+  'loglevel' => 2,
+  'trashbin_retention_obligation' => 'auto',
 );
